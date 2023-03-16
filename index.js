86c6345c2eb0aa762b99b2dd5505912cc91d7236
khanhtq99 <khanh.truong@ekoios.vn>
2023-03-16 15:08:53 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const name = {
+  a: "khanh",
+};
+
+const age = {
+  a: "khanh",
+};
+
+console.log("so sánh", name.a === age.a);
+console.log("ss object == :>> ", name == age);
+console.log("ss object === :>> ", name === age);
+
+const a = [1, 2, 3];
+const b = [1, 2, 3];
+
+console.log("a === b :>> ", a === b);
